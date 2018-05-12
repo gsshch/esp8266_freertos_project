9a4f4bc6ac970ef8e232030a3c96ecdd139c4051
gsshch <1729719432@qq.com>
2018-05-12 17:58:59 +0800

    add gpio intr func;

--- examples/smart_config/user/process_apds9960.c
+++ examples/smart_config/user/process_apds9960.c
@@ -9,6 +9,8 @@
 #include "c_types.h"
 #include "i2c_master.h"
 #include "esp_libc.h"
+#include "task.h"
+#include "gpio_register.h"
 
 #define ESP_DBG os_printf
 
@@ -57,10 +59,42 @@ void ICACHE_FLASH_ATTR apds9960_task(void *pvParameters) {
 	}
 }
 
+#define apds_intr_pin 13
+
+void intr_cb_func() {
+	//可控硅中断触发;
+	void siliconControlIsrHandler() {
+		u32 pin_status = 0;
+		/** 读取GPIO中断状态可以判断是那个端口的中断 */
+		pin_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
+		ETS_GPIO_INTR_DISABLE();/** 关闭GPIO中断 */
+		if (pin_status & BIT(apds_intr_pin)) /*GPIO中断*/
+		{
+			INT_Flag = true;
+		}
+
+		GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, pin_status); /** 清除GPIO中断标志 */
+		ETS_GPIO_INTR_ENABLE();
+	}
+}
+
+void ICACHE_FLASH_ATTR intr_event_init() {
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13);    //定义管教功能  作GPIO使用
+	GPIO_DIS_OUTPUT(FUNC_GPIO13);  //使能输出功能
+	//GPIO13中断配置
+	ETS_GPIO_INTR_DISABLE(); 										//中断失能
+	ETS_GPIO_INTR_ATTACH(&intr_cb_func, NULL);    		//注册中断函数
+	gpio_pin_intr_state_set(GPIO_ID_PIN(apds_intr_pin), GPIO_PIN_INTR_ANYEDGE); //设置中断方式
+	GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(apds_intr_pin)); //清除该引脚的GPIO中断标志
+	ETS_GPIO_INTR_ENABLE(); 										//中断使能
+}
+
 void ICACHE_FLASH_ATTR apds_process_init() {
+
 	i2c.i2c_master_gpio_init();
 	APDS9960_Init();
 	APDS9960_enableGestureSensor(true);
-
+	intr_event_init();
+	xTaskCreate(apds9960_task, "apds9960_task", 256, NULL, 2, NULL);
 }
 
